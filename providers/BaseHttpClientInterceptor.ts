e9101e03c5a94ec716e1a7c8ced7c482b34d3171
danyducky <morikov2000@gmail.com>
2021-11-22 01:11:15 -0700

    Правки;

--- providers/BaseHttpClientInterceptor.ts
+++ providers/BaseHttpClientInterceptor.ts
@@ -1,12 +1,13 @@
 import {Injectable} from '@angular/core';
 import {HttpEvent, HttpInterceptor, HttpHandler, HttpRequest} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {environment} from "../src/environments/environment";
 
 @Injectable()
 export class BaseHttpClientInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-    const apiReq = req.clone({ url: `https://radiant-crag-33900.herokuapp.com/${req.url}` });
+    const apiReq = req.clone({ url: `${environment.apiUrl}/${req.url}` });
     return next.handle(apiReq);
   }
 }
